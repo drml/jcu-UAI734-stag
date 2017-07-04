7760c0b1487d294093dd4afd4079219307f13994
Drml <medicus>
2017-07-04 08:41:01 +0200

    INIT Maven projekt zalozen.

---
+++ src/main/java/cz/jcu/uai/javapract/App.java
@@ -0,0 +1,13 @@
+package cz.jcu.uai.javapract;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
