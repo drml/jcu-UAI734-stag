8f691daf1c9d149a0117b22c5ebb95192000eddb
Drml <medicus>
2017-07-04 10:31:03 +0200

    ENUM pro predmety

---
+++ src/main/java/cz/jcu/uai/javapract/SubjectType.java
@@ -0,0 +1,26 @@
+package cz.jcu.uai.javapract;
+
+/**
+ * Represents type of the subject
+ *
+ * Created by Drml on 4.7.2017.
+ */
+public enum SubjectType {
+    CVICENI("Cv"),
+    PREDNASKA("Pr");
+
+    private String represendation;
+
+    SubjectType(String represendation)
+    {
+        this.represendation = represendation;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "SubjectType{" +
+                "represendation='" + represendation + '\'' +
+                '}';
+    }
+}
